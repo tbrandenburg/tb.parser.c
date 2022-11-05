97f769c29ab637dc9669ca83c7319b2897bd99bb
Tom Brandenburg <tom.brandenburg@gmx.de>
2022-11-05 17:30:09 +0100

    Updated make script

--- make.sh
+++ make.sh
@@ -3,5 +3,5 @@ cd external/JSON/src/main/java/
 echo Compiling...
 javac org/json/*.java
 echo Creating JAR...
-jar cf ../../../bin/json.jar org/json/*.class
+jar cf ../../../json.jar org/json/*.class
 cd ../../../../../
